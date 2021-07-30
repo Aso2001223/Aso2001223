7fdc3adca1c25a662446d5c442f7cd94fc4c1d2e
Aso2001223 <83046809+Aso2001223@users.noreply.github.com>
2021-07-30 22:48:31 +0900

    Create Main713.java

---
+++ Main713.java
@@ -0,0 +1,9 @@
+public class Main713{
+    public static void main(String[] args){
+        System.out.println("***************************");
+        System.out.println("         戦艦ゲーム         ");
+        System.out.println("***************************");
+        Game713 game = new Game713();
+        game.display();
+    }
+}
